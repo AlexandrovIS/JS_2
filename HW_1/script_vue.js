d659ea3b82bf1e1c428195c38c1f3288d38a9f87
Ivan <alexandrovis@gmail.com>
2021-03-25 14:32:28 +0300

    message

--- HW_1/script_vue.js
+++ HW_1/script_vue.js
@@ -7,7 +7,7 @@ const vue = new Vue({
     filtredGoods: [],
     bin: [],
     isBinOpen: false,
-    quarel: 100,
+    quarel: 1,
     search: ''
   },
   methods: {
@@ -33,7 +33,7 @@ const vue = new Vue({
           if (xhr.status === 200) {
             success(JSON.parse(xhr.responseText))
           } else if (xhr.status > 400) {
-            error('временно недоступно')
+            error(`<h3>We appologize for it</h3>`)
           }
         }
       }
@@ -52,8 +52,15 @@ const vue = new Vue({
     },
 
     addToBin(event) {
+      // this.bin.forEach((item) => {
+      //   if (item.title === event.target.parentElement.querySelector('h3').textContent) {
+      //     console.log('1')
+      //   }
+      // })
+
       const index = event.target.dataset.index
       this.bin.push(this.filtredGoods[index])
+
     },
     removeBinHandler(event) {
       const index = event.target.dataset.index
@@ -63,6 +70,10 @@ const vue = new Vue({
       return this.bin.reduce((prev, currentValue) => {
         return prev + currentValue.price
       }, 0)
+    },
+
+    imgAlt(index) {
+      return "IMG_" + this.filtredGoods[index].title
     }
   },
 
@@ -73,7 +84,7 @@ const vue = new Vue({
         this.filtredGoods = data
       })
       .catch(err => {
-        console.log(err)
+        document.querySelector('.goods-list').innerHTML = err
       })
   }
 
