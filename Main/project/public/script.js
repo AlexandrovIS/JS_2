3fda973ee4d9ba3938bea5c6b82abb5066c4d899
Ivan <alexandrovis@gmail.com>
2021-04-02 11:10:49 +0300

    hw7

--- Main/project/public/script.js
+++ Main/project/public/script.js
@@ -1,5 +1,3 @@
-const API_URL = 'https://raw.githubusercontent.com/DeevMaks/GB_java_script_part_2/main/goods.json'
-
 Vue.component('goods-item', {
   template: `<div :data-id="id" class="goods-item" >
   <h3>{{title}}</h3>
@@ -28,33 +26,6 @@ Vue.component('goods-item-cart', {
   }
 })
 
-Vue.component('cart', {
-  template: `<div>
-  <button class="cart-button" @click="openCartHandler" type="button">
-  BIN <span v-if="cart_arr.length>0">{{cart_arr.length}}</span></button> 
-  <div v-if="isVisibleCart">
-  <slot> </slot>
-  <h3>SUM <span v-if="cartSum()>0">{{cartSum()}}</span></h3>
-  </div>
-  </div>`,
-  data() {
-    return {
-      isVisibleCart: true,
-    }
-  },
-  props: {
-    cart_arr: Array
-  },
-  methods: {
-    openCartHandler() {
-      this.isVisibleCart = !this.isVisibleCart
-    },
-    cartSum() {
-      return this.cart_arr.reduce((acc, current) => { return acc + current.price }, 0)
-    }
-  }
-})
-
 Vue.component('search', {
   template: `<input @input="check" v-model="search" placeholder="ПОИСК">`,
   data() {
@@ -79,18 +50,58 @@ Vue.component('search', {
   }
 })
 
+Vue.component('cart', {
+  template: `<div>
+   <search v-bind:goods_arr="arr" @filtred="filterHandler"></search>
+  <button class="cart-button" @click="openCartHandler" type="button">
+  BIN <span v-if="cart_arr.length>0">{{cart_arr.length}}</span></button> 
+  <div v-if="isVisibleCart">
+  <slot> </slot>
+  <h3>SUM <span v-if="cartSum()>0">{{cartSum()}}</span></h3>
+  </div>
+  </div>`,
+  data() {
+    return {
+      isVisibleCart: false,
+    }
+  },
+  props: {
+    cart_arr: Array,
+    arr: Array
+  },
+  methods: {
+    openCartHandler() {
+      this.isVisibleCart = !this.isVisibleCart
+    },
+    cartSum() {
+      return this.cart_arr.reduce((acc, current) => { return acc + current.price }, 0)
+    },
+    filterHandler(arrFiltered) {
+      this.$emit('filter', arrFiltered)
+    }
+  }
+})
+
 const vue = new Vue({
   el: '#app',
   data: {
     cart: [],
     goods: [],
     filtredGoods: [],
+    isLoaded: false,
   },
   methods: {
     addToCartHandler(e) {
-
       const id = e.target.closest('.goods-item').dataset.id
       const good = this.goods.find((item) => item.id == id)
+
+      fetch('/cart', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(good)
+      })
       this.cart.push(good)
 
     },
@@ -100,54 +111,28 @@ const vue = new Vue({
       this.cart.splice(goodIndex, 1)
     },
     searchHandler(e) {
-      // const {
-      //   target: { value },
-      // } = e;
-      // if (value === "") {
-      //   this.filtredGoods = this.goods;
-      // }
-      // const regexp = new RegExp(value, "gi");
-      // this.filtredGoods = this.goods.filter((good) => regexp.test(good.title));
       this.filtredGoods = e
-
-    },
-
-    fetch(error, success) {
-      let xhr
-
-      if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest()
-      } else if (window.ActiveXObject) {
-        xhr = new ActiveXObject('Microsoft.XMLHTTP')
-      }
-
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            success(JSON.parse(xhr.responseText))
-          } else if (xhr.status > 400) {
-            error(`<h3>We appologize for it</h3>`)
-          }
-        }
-      }
-      xhr.open('GET', API_URL, true)
-      xhr.send()
-    },
-
-    fetchPromise() {
-      return new Promise((resolve, reject) => {
-        this.fetch(reject, resolve)
-      })
     }
   },
   mounted() {
-    this.fetchPromise()
+    fetch('/data')
+      .then(response => response.json())
       .then(data => {
         this.goods = data
         this.filtredGoods = data
+        this.isLoaded = true
       })
       .catch(err => {
         document.querySelector('.goods-list').innerHTML = err
+      })
+
+    fetch('/cart')
+      .then(response => response.json())
+      .then(data => {
+        this.cart = data
+      })
+      .catch(err => {
+        console.log(err)
       })
   }
 })
